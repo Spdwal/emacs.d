abb4fa274b378e3e41713e92e81c0a58d42b4945
spdwal <568741362@qq.com>
2021-03-22 13:16:42 +0800

    add split windows config in ansi-term

--- lisp/init-term.el
+++ lisp/init-term.el
@@ -2,9 +2,24 @@
   :hook (
 	 (term-mode . eterm-256color-mode)
 	 (term-mode . (lambda ()
-		       (display-line-numbers-mode 0))))
-  :bind
-  ("C-c t" . ansi-term))
+		       (display-line-numbers-mode 0)))))
+    
+;; 为 zsh 提供窗口模式
+(defun ns/my-term-p (name _action)
+  "Determine whether NAME names a `term-mode' buffer."
+  (with-current-buffer name
+    (derived-mode-p #'term-mode)))
+
+;; 直接打开 zsh
+(defun zsh ()
+  (interactive)
+  (let ((switch-to-buffer-obey-display-actions t))
+    (ansi-term "zsh")))
+
+(add-to-list 'display-buffer-alist '(ns/my-term-p () (inhibit-same-window . t)))
+
+;; 设置 zsh 快捷键
+(global-set-key (kbd "C-c t") 'zsh)
 
 (provide 'init-term)
 
